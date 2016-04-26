c53e0748b2a005c826b41765f88319235db79549
Death <deathmetalland@gmail.com>
2016-04-26 23:47:26 +0900

    youtube_sample: 録画した時間を stat.ink に送信
    fixes #31

--- run_upload_video_ikalog.sh
+++ run_upload_video_ikalog.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # ファイル名から時間の抽出
-# この時間は動画ファイル作成時間だからここからプレイ時間を減算すれば録画開始時間になるか？
-#HH=`basename "$1" .mov | cut -c 19-20`
-#mm=`basename "$1" .mov | cut -c 21-22`
-#echo $HH:$mm
+epoch_time=`basename "$1" .mov | cut -c 10-22`
+epoch_time="${epoch_time}00"
+echo $epoch_time
 
 python upload_video.py --file="$1" --title="Splatoon" --privacyStatus="public" > upload_video.log
 VIDEOID=`tail -n 1 upload_video.log`
-/usr/local/bin/python3 IkaLog.py --input_file "$1" --video_id $VIDEOID
+/usr/local/bin/python3 IkaLog.py --input_file "$1" --video_id $VIDEOID --epoch_time $epoch_time
