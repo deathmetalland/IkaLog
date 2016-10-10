3029839f51e185400a20bc64dd27fb6828a97d2c
Death <deathmetalland@gmail.com>
2016-10-11 00:44:00 +0900

    Change timestamp by script
    Signed-off-by: Death <deathmetalland@gmail.com>

--- run_upload_video_ikalog.sh
+++ run_upload_video_ikalog.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # ファイル名から時間の抽出
-epoch_time=`basename "$1" .m4v | cut -c 10-22`
-epoch_time="${epoch_time}00"
+epoch_time=`basename "$1" .m4v | cut -c 12-22`
 echo $epoch_time
+
+# タイムスタンプを変更
+touch -t $epoch_time "$1"
 
 python upload_video.py --file="$1" --title="Splatoon" --privacyStatus="public" > upload_video.log
 VIDEOID=`tail -n 1 upload_video.log`
-/usr/local/bin/python3 IkaLog.py --input_file "$1" --video_id $VIDEOID --epoch_time $epoch_time
+/usr/local/bin/python3 IkaLog.py --input_file "$1" --video_id $VIDEOID
