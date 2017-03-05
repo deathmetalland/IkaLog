3071767f28a1db34e30c1104792b084e6512dc1f
Death <deathmetalland@gmail.com>
2017-03-06 01:07:35 +0900

    Change upload description

--- run_upload_video_ikalog.sh
+++ run_upload_video_ikalog.sh
@@ -7,6 +7,13 @@ echo $epoch_time
 # タイムスタンプを変更
 touch -t $epoch_time "$1"
 
-python upload_video.py --file="$1" --title="Splatoon" --privacyStatus="public" > upload_video.log
+python upload_video.py \
+--file="$1" \
+--title="Splatoon" \
+--description="私は、Nintendo Creators Programのライセンスによって、この動画で任天堂コンテンツを使用しています。この動画は、任天堂による援助や支援がなされているものではありませんが、この動画から得られる広告収益は任天堂と分け合われます。" \
+--keywords="Splatoon" \
+--privacyStatus="public" \
+> upload_video.log
+
 VIDEOID=`tail -n 1 upload_video.log`
 /usr/local/bin/python3 IkaLog.py --input_file "$1" --video_id $VIDEOID
